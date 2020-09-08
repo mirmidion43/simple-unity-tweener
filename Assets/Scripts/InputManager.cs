7a3ebb4a517092113eef6c70b1c7f33184513060
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-09-08 13:49:26 +1000

    initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject item;
+    private Tweener tweener;
+    private List<GameObject> itemList = new List<GameObject>();
+
+    int count = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = gameObject.GetComponent<Tweener>();
+        itemList.Add(item);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Transform currentTrans = item.transform;
+        //Vector3 currentPos = item.transform.position;
+        Vector3 basePos = new Vector3(0.0f, 0.5f, 0.0f);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //GameObject item = Instantiate(item, basePos, transform.rotation);
+            itemList.Add(Instantiate(item, basePos, transform.rotation));
+            Debug.Log(itemList.Count);
+        }
+
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                foreach(GameObject item in itemList)
+                {
+                    if(tweener.AddTween(item.transform, item.transform.position, new Vector3(-2.0f,0.5f,0.0f), 1.5f))
+                    break;
+                }
+            }
+                         
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                foreach(GameObject item in itemList)
+                {
+                    if(tweener.AddTween(item.transform, item.transform.position, new Vector3(2.0f,0.5f,0.0f), 1.5f))
+                    break;
+                }              
+            }
+                
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                foreach(GameObject item in itemList)
+                {
+                    if(tweener.AddTween(item.transform, item.transform.position, new Vector3(0.0f,0.5f,-2.0f), 0.5f))
+                    break; 
+                }               
+            }
+                         
+            if(Input.GetKeyDown(KeyCode.W))
+            {
+                foreach(GameObject item in itemList)
+                {
+                    if(tweener.AddTween(item.transform, item.transform.position, new Vector3(0.0f,0.5f,2.0f), 0.5f))
+                    break;
+                } 
+            }
+               
+        
+    }
+}
---
+++ Assets/Scripts/Tweener.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    //private Tween activeTween;
+    private List<Tween> activeTweens = new List<Tween>();
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if(TweenExists(targetObject))
+            return false;
+        else
+            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time, duration));
+        return true;
+    }
+
+    public bool TweenExists(Transform target)
+    {
+        foreach(Tween Tween in activeTweens)
+        if(Tween.Target == target)
+        {
+            return true;
+        }
+      
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeTweens != null)
+        
+        //foreach(Tween Tween in activeTweens)
+        for(int i = activeTweens.Count - 1; i >= 0; i--)
+        {
+                float distance = Vector3.Distance(activeTweens[i].Target.position, activeTweens[i].EndPos);
+
+                if(distance > 0.1f)
+                {
+                    float t = (Time.time - activeTweens[i].StartTime) / activeTweens[i].Duration;
+                    float cubeT = t*t*t;
+                    activeTweens[i].Target.position = Vector3.Lerp(activeTweens[i].StartPos, activeTweens[i].EndPos, cubeT);
+                }
+        
+                if(distance <= 0.1f)
+                {
+                    activeTweens[i].Target.position = activeTweens[i].EndPos;
+                    activeTweens.Remove(activeTweens[i]);
+                } 
+        }
+        
+
+    }
+}
